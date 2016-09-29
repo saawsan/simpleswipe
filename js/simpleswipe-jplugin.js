64a044f2fa574734277016a75fda659e228a37ee
saawsan <s.cheffah@gmail.com>
2016-09-29 18:09:54 +0200

    CURRENT - debug swipe on Android device. Touchcancel fires as soon as touchstart is sent. A preventDefault() on touchmove event corrects the error but native scroll is canceled. Workaround: re-implement scroll with JS.

--- js/simpleswipe-jplugin.js
+++ js/simpleswipe-jplugin.js
@@ -1,5 +1,18 @@
 /*jshint browser:true, curly:true, eqeqeq:false, forin:true, strict:false, undef:true*/ 
 /*global jQuery:false, $:false*/
+
+var createConsoleLog = function() {
+    var iDiv = document.createElement('div');
+    iDiv.id = 'console';
+    document.getElementsByTagName('body')[0].appendChild(iDiv);
+};
+var consolelog = function(sMsg) {
+    console.log(sMsg);
+    var consoleElm = document.getElementById('console');
+    consoleElm.innerHTML =  sMsg + '<br>' + consoleElm.innerHTML;
+};
+createConsoleLog();
+
 ;(function ($) {
     var pluginName = 'simpleswipe';
     var defaults =  {
@@ -14,6 +27,8 @@
 
         this.touchStartX = 0;
         this.touchEndX = 0;
+        this.touchStartY = 0;
+        this.touchEndY = 0;
         this.mouseStartX = 0;
         this.mouseEndX = 0;
         this.isMoving = false;
@@ -31,22 +46,6 @@
             var oCfg = this;
             var elm = oCfg.element;
 
-            if('ontouchstart' in window){
-                elm.addEventListener('touchstart', function(event) {
-                    oCfg.touchStartX = event.touches[0].pageX;
-                }, false);
-
-                elm.addEventListener('touchmove',function(event) {
-                    oCfg.touchEndX = event.touches[0].pageX;
-                    oCfg.isMoving = true;
-                },false);
-
-                elm.addEventListener('touchend', function() {
-                    oCfg.isMoving = false;
-                    $(elm).trigger(oCfg.getSwipeDirection('touch'));
-                }, false);
-            }
-
             /*TODO IE*/
             // if (window.navigator.msPointerEnabled) {
                 // elm.addEventListener('MSPointerDown', function(event) {
@@ -63,6 +62,31 @@
                 // }, false);
             // }
 
+            /*Handle Touch Events*/
+            if('ontouchstart' in window){
+                elm.addEventListener('touchstart', function(event) {
+                    oCfg.touchStartX = event.touches[0].pageX;
+                    oCfg.touchStartY = event.touches[0].pageY;
+                }, false);
+
+                elm.addEventListener('touchmove',function(event) {
+                    event.preventDefault();
+                    oCfg.touchEndX = event.touches[0].pageX;
+                    oCfg.touchEndY = event.touches[0].pageY;
+                    oCfg.isMoving = true;
+                },false);
+
+                elm.addEventListener('touchend', function() {
+                    oCfg.isMoving = false;
+                    $(elm).trigger(oCfg.getSwipeDirection('touch'));
+                }, false);
+                elm.addEventListener('touchcancel', function() {
+                    oCfg.isMoving = false;
+                    $(elm).trigger(oCfg.getSwipeDirection('touch'));
+                }, false);
+            }
+
+            /*Handle Mouse Events*/
             elm.addEventListener('mousedown', function(event) {
                 oCfg.mouseStartX = event.clientX;
             }, false);
@@ -80,12 +104,34 @@
             if(!sSwipeType) { return; }
             
             var oCfg = this;
-            var orientation, nDifference;
+            var orientation, nDifference, nDifferenceY;
             
             var nStartX = (sSwipeType == 'touch') ? oCfg.touchStartX : oCfg.mouseStartX;
             var nEndX = (sSwipeType == 'touch') ? oCfg.touchEndX : oCfg.mouseEndX;
 
-            if(nStartX && nEndX){
+            /*test if touch need to be a scroll?*/
+            nDifferenceY = oCfg.touchEndY - oCfg.touchStartY;
+
+            if(nDifferenceY > 100 || nDifferenceY < -100) {
+                //USER IS SCROLLING > do not swipe
+                // window.scrollBy(0, -nDifferenceY);
+                var nCountInterval = 25;
+                var nCountPixelInterval = nDifferenceY/nCountInterval;
+                var nCountDelay = 800/nCountInterval;
+                consolelog('A')
+                //is setInterval asynchronous??
+                var oIntervalScroll = setInterval(function() {
+                    consolelog('B')
+                    nCountInterval--;
+                    if(nCountInterval === 0){
+                        clearInterval(oIntervalScroll);
+                        return;
+                    }
+                    window.scrollBy(0, -nCountPixelInterval);
+                }, nCountDelay);
+                consolelog('F')
+            } else {
+                //USER IS NOT SCROLLING > Swipe
                 nDifference = nEndX - nStartX;
                 if(nEndX > nStartX){ //right
                     orientation = (nDifference < oCfg.settings.longSwipeMin) ? 'swiperight': 'swipelongright';
@@ -93,6 +139,7 @@
                     orientation = (nDifference > (-oCfg.settings.longSwipeMin)) ? 'swipeleft': 'swipelongleft';
                 }
             }
+            consolelog(orientation)
             return orientation;
         }
     };
